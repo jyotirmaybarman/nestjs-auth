092047af65eca10577cdbed25a5bc504bc954e31
Jyotirmay Barman <jyotirmay@developerzilla.com>
2023-01-23 18:55:25 +0530

    add: config module to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './providers/database/prisma/prisma.module';
 import { UsersModule } from './models/users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UsersModule]
+  imports: [PrismaModule, UsersModule, ConfigModule.forRoot({ isGlobal: true })]
 })
 export class AppModule {}
