5d82ba7a99095ba984bf88217fadacd17783fc68
Jyotirmay Barman <jyotirmay@developerzilla.com>
2023-01-23 15:33:19 +0530

    add: user schema & migration

---
+++ src/providers/database/prisma/migrations/20230123072212_create_users_table/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "verification_token" TEXT,
+    "reset_token" TEXT,
+    "new_email" TEXT,
+    "role" "roles" NOT NULL DEFAULT 'user',
+    "avatar" TEXT,
+    "bio" TEXT NOT NULL DEFAULT '',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
